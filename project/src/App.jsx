bcf5e902f05f494cd7985c707bcc1d05173b5843
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-18 17:36:50 +0530

    added a button

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -7,16 +7,21 @@ function App() {
       <div className='container'>
         <div className="header">
           {/*image */}
-          
+          <div className="div-right">
+
+          </div>
+
           {/*navigation bar*/}
-          <div className="nav">
+          <div className="nav-left">
             <a href="#">SERVICES</a>
             <a href="#">ABOUT US</a>
             <a href="#">CONTACT US</a>
             <a href="#">CAREERS</a>
           </div>
-
         </div>
+
+        {/*button */}
+        <button className='button'>Get free consulation</button>
         
       </div>
  
